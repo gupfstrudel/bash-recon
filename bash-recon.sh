0059e6f45a8102caf16c77acb3900646971825bf
Lena-Marie <hyperleni@gmail.com>
2025-07-03 20:51:39 +0200

    some codedesign stuff

--- bash-recon.sh
+++ bash-recon.sh
@@ -14,18 +14,18 @@
  echo "[*] Whois:" | tee -a $OUT
  whois $TARGET | head -n 20 >> $OUT
 
-echo "[*] DNS info:" | tee -a $OUT
-dig $TARGET ANY +short >> $OUT
+ echo "[*] DNS info:" | tee -a $OUT
+ dig $TARGET ANY +short >> $OUT
 
-echo "[*] subdomains:" | tee -a $OUT
-for sub in www mail ftp test dev; do
- host "$sub.$TARGET" | grep "has address" >> $OUT
-done
+ echo "[*] subdomains:" | tee -a $OUT
+ for sub in www mail ftp test dev; do
+  host "$sub.$TARGET" | grep "has address" >> $OUT
+ done
 
-echo "[*] nmap scan top 1000 ports" | tee -a $OUT
-nmap -T4 -F $TARGET >> $OUT
+ echo "[*] nmap scan top 1000 ports" | tee -a $OUT
+ nmap -T4 -F $TARGET >> $OUT
 
-echo "[*] directory scan" | tee -a $OUT
-gobuster dir -u http://$TARGET -w /usr/share/wordlists/dirb/common.txt -q -t 20 >> $OUT
+ echo "[*] directory scan" | tee -a $OUT
+ gobuster dir -u http://$TARGET -w /usr/share/wordlists/dirb/common.txt -q -t 20 >> $OUT
 
-echo "[*] Recon Complete. Check $OUT"
+ echo "[*] Recon Complete. Check $OUT"
